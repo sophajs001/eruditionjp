16fb1ddda377e8bd632e33fb0e0c549326db0bbf
sophajs001 <sophajsgtech@gmail.com>
2026-06-28 04:40:56 +0100

    Revert last commit and restore build output

--- .nuxt/dist/server/_nuxt/welcome-styles.Az6SPJS_.mjs
+++
@@ -1,4 +0,0 @@
-import style_0 from "./welcome-styles-1.mjs-D8jt3CL4.js";
-export default [
-  style_0
-]
--- .nuxt/dist/server/styles.mjs
+++
@@ -1,6 +0,0 @@
-const interopDefault = r => r.default || r || []
-export default {
-  "node_modules/nuxt/dist/app/components/welcome.vue": () => import('./_nuxt/welcome-styles.Az6SPJS_.mjs').then(interopDefault),
-  "node_modules/nuxt/dist/app/components/error-404.vue": () => import('./_nuxt/error-404-styles.xf2R7Kcb.mjs').then(interopDefault),
-  "node_modules/nuxt/dist/app/components/error-500.vue": () => import('./_nuxt/error-500-styles.CkOlAevR.mjs').then(interopDefault)
-}
